7047ae974e12d0979d9c9f0c66afeca5455f0f92
tayyab80 <tayyab80@Outlook.com>
2018-09-02 12:09:48 +0500

    Advanced Lists

--- src/pages/dishdetail/dishdetail.module.ts
+++ src/pages/dishdetail/dishdetail.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { DishdetailPage } from './dishdetail';
 
+
 @NgModule({
   declarations: [
     DishdetailPage,
@@ -11,3 +12,4 @@ import { DishdetailPage } from './dishdetail';
   ],
 })
 export class DishdetailPageModule {}
+
